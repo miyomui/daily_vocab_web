da27b67ac0f757c0495889a9b6330e405aa8d75d
miyomui <lalittawadeevongkoon@gmail.com>
2025-11-21 16:58:52 +0700

    Update

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -1,8 +1,15 @@
 import { NextResponse } from "next/server";
 import { words } from "@/data/words";
 
+// export async function GET() {
+//   const randomIndex = Math.floor(Math.random() * words.length);
+//   const word = words[randomIndex];
+//   console.log('code was run.');  
+//   return NextResponse.json({ data: word });
+// }
+
 export async function GET() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const word = words[randomIndex];
-  return NextResponse.json({ word });
+  const response = await fetch('http://localhost:8000')
+  const data = await response.json();
+  return NextResponse.json({data}); 
 }
